82bcfa1ed0d4135c85d4a5d737f8ed12ab73b2c6
edwinmacalopu <khiwendz@gmail.com>
2020-07-19 21:35:27 -0500

    :cyclone:add CI/CD github Actions

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_rule/src/bloc/bloc.dart';
-import 'package:google_maps_rule/src/pages/Gmaps_page.dart';
 import 'package:provider/provider.dart';
- 
+import 'package:google_maps_rule/src/pages/Gmaps_page.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-            providers: [
-              ChangeNotifierProvider(create: (_)=>ProviderMaps())
-            ],
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
+      providers: [ChangeNotifierProvider(create: (_) => ProviderMaps())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
-    buttonTheme:ButtonThemeData(
-     buttonColor: Colors.blueAccent,
-     height:50,
-     textTheme: ButtonTextTheme.primary,
-     minWidth: 80,
-     shape: RoundedRectangleBorder(
-       borderRadius: BorderRadius.circular(10)
-     ),
-
-   ) 
-        ),
+            buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent,
+          height: 50,
+          textTheme: ButtonTextTheme.primary,
+          minWidth: 80,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        )),
         home: GmapsPage(),
       ),
     );
